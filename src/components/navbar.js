62f0e41db2bbe03aeb86c9f2cf1fce2e8cf778df
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-03-18 17:32:09 +0100

    feat: removed dropdown from navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,17 +25,7 @@ export const Header = () => {
           <Nav className="me-auto">
           <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/schedule">Schedule</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -49,4 +39,16 @@ export const Header = () => {
 }
 
 
-
+/*
+<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            */
