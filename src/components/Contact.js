8d5e006a92de4ac84aa08e7296bf175f2fca15f7
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-03-18 16:57:39 +0100

    refactor: modularized code for home screen

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact = () => {
+	return (
+		<div>
+			<p>Please email us at <a href="mailto:nysl@chisoccer.org">nysl@chisoccer.org</a></p>
+			<p id='contact-bottom'>We will reply to your email as soon as we can.</p>
+		</div>
+	)
+}
--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,10 @@ import { Schedule } from './schedule';
 import { Container } from 'react-bootstrap';
 import Photo from '../utils/asset/design1_image1.jpg';
 import Image from 'react-bootstrap/Image';
+import { Picture } from './Picture';
+import { HeadingText } from './HeadingText';
+import { About } from './About';
+import { Contact } from './Contact';
 
 
 
@@ -20,48 +24,10 @@ export const Home = () => {
 }
 
 
-const Picture = () => {
-  return <Image src={Photo} fluid />;
-}
 
-const HeadingText = () => {
-	return (
-		<div>
-			<h1>Northside Youth Soccer League</h1>
-		</div>
-	)
-}
 
-const About = () => {
-	return (
-			<div className='about-div'>
-			<h3>Mission</h3>
-			<p>To support young athletes living in Chicago's northside
-			neighborhoods, who have an interest in learning and playing
-			soccer, with opportunities to learn and practice skills related
-			to the game of soccer, specifically those skills around team
-			cooperation and good sportsmanship.</p>
-			<h3>Vision</h3>
-			<p>The Northside Youth Soccer League aspires to develop strong,
-			well-rounded, and mindful athletes through the building of
-			character, self-discipline, and leadership.</p>
-			<h3>General Information</h3>
-			<p>The Northside Youth Soccer League was established in 1996 to
-			provide athletes residing in Chicago's northside neighborhoods an
-			environment in which to learn and play soccer. To be a member of
-			NYSL, you must be between the ages of 4 - 12 and reside in a Chicago
-			northside neighborhood. NYSL is ran by a small full-time staff, and
-			relies on the generous volunteer time of parents and previous league
-			members.</p>
-		</div>
-	)
-}
 
-const Contact = () => {
-	return (
-		<div>
-			<p>Please email us at <a href="mailto:nysl@chisoccer.org">nysl@chisoccer.org</a></p>
-			<p id='contact-bottom'>We will reply to your email as soon as we can.</p>
-		</div>
-	)
-}
+
+
+
+
