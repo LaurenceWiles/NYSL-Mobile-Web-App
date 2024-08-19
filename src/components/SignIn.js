efecb0b43b33fd2bc81371e5ac3b91c8d3144b1a
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-08-19 14:52:05 +0200

    updated navbar to dynamically set class name of the sign-in and log-out buttons so their size is small when navbar is collapsed

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { auth, provider } from "./firebase";
 import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { useMediaQuery } from "react-responsive";
 
 export const SignIn = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const isSmallScreen = useMediaQuery({ maxWidth: 425 });
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -33,13 +36,21 @@ export const SignIn = () => {
   return (
     <div>
       {!isSignedIn && (
-        <Button variant="secondary" onClick={signInWithGoogle}>
+        <Button
+          variant="secondary"
+          onClick={signInWithGoogle}
+          className={isSmallScreen ? "btn-sm" : ""}
+        >
           {" "}
           Sign In
         </Button>
       )}
       {isSignedIn && (
-        <Button variant="secondary" onClick={logOut}>
+        <Button
+          variant="secondary"
+          onClick={logOut}
+          className={isSmallScreen ? "btn-sm" : ""}
+        >
           Log Out
         </Button>
       )}
