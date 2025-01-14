ab8928e52382fde788cc662a2400b9f1ac092afd
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-01-14 20:36:00 +0100

    renamed navbar.js to fit with capital letter naming conventions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./views/Home";
 import { Schedule } from "./components/schedule";
-import { Header } from "./components/navbar";
+import { Header } from "./components/Navbar";
 import { GameDetails } from "./views/GameDetails";
 import { SignIn } from "./components/SignIn";
 import { Messages } from "./views/Messages";
