786dcbc0cc55c0e2eb8f3b365a2a0f3bfe37c3e1
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-01-24 18:50:35 +0100

    refactor: removed redundant home link from NavHeader

--- src/components/HomePicture.js
+++ src/components/HomePicture.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "react-bootstrap/Image";
 import Photo from "../utils/asset/design1_image1.jpg";
 
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -31,9 +31,6 @@ export const NavHeader = ({ onNavbarToggle }) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
               <Nav.Link as={Link} to="/schedule">
                 Schedule
               </Nav.Link>
