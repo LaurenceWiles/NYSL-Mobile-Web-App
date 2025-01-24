de68a6ea419db3615daa7a262207bce34e0e7004
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-01-24 16:26:19 +0100

    refactor: removed unnecessary BrowserRouter import from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./views/Home";
 import { Schedule } from "./views/Schedule";
