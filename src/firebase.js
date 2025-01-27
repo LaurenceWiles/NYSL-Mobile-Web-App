96c91bb0a1f07242ad8f3cd3ffb122bc6a121f38
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-01-27 12:26:20 +0100

    removed unused analytics variable from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getAuth, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -15,7 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
