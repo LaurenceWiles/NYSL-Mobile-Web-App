2098b6541647b7bd61495a8d667a3db4a7b56466
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-01-31 16:59:43 +0100

    refactor: moved handleFileSelect logic from UploadPicture component into FileInput component

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,15 +1,22 @@
 import { Form } from "react-bootstrap";
 
-const FileInput = ({ fileInputRef, onFileSelect }) => (
-  <Form.Group controlId="formFile" className="mb-3">
-    <Form.Control
-      ref={fileInputRef}
-      type="file"
-      accept="image/*"
-      capture="environment"
-      onChange={onFileSelect}
-    />
-  </Form.Group>
-);
+const FileInput = ({ fileInputRef, setSelectedImage }) => {
+  const handleFileSelect = (event) => {
+    const selectedFile = event.target.files[0];
+    setSelectedImage(selectedFile);
+  };
+
+  return (
+    <Form.Group controlId="formFile" className="mb-3">
+      <Form.Control
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        capture="environment"
+        onChange={handleFileSelect}
+      />
+    </Form.Group>
+  );
+};
 
 export default FileInput;
--- src/components/UploadPicture.js
+++ src/components/UploadPicture.js
@@ -1,12 +1,6 @@
 import { useState, useRef } from "react";
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  getMetadata,
-} from "firebase/storage";
-import { storage } from "../firebase";
 import { Alert } from "react-bootstrap";
+import useUpload from "../hooks/useUpload";
 import FileInput from "./FileInput";
 import ImagePreview from "./ImagePreview";
 import PostButton from "./PostButton";
@@ -14,43 +8,21 @@ import CancelButton from "./CancelButton";
 
 const UploadPicture = ({ gameId, user, onUpload }) => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [uploading, setUploading] = useState(false);
-  const [error, setError] = useState(null);
   const fileInputRef = useRef(null);
 
-  const handleFileSelect = (event) => {
-    const selectedFile = event.target.files[0];
-    setSelectedImage(selectedFile);
-    setError(null);
-  };
+  const { uploadImage, uploading, error } = useUpload(gameId, user, onUpload);
 
-  const handleUpload = async () => {
-    if (selectedImage && user) {
-      setUploading(true);
-      const gameRef = ref(storage, `pictures/${gameId}/${selectedImage.name}`);
-      try {
-        const metadata = {
-          customMetadata: {
-            user: user.email,
-          },
-        };
-        await uploadBytes(gameRef, selectedImage, metadata);
-        const newImageUrl = await getDownloadURL(gameRef);
-        const newImageMetadata = await getMetadata(gameRef);
-        onUpload({ url: newImageUrl, metadata: newImageMetadata });
-        setSelectedImage(null);
-        if (fileInputRef.current) fileInputRef.current.value = "";
-      } catch (error) {
-        console.error("Error uploading file:", error);
-        setError("Error uploading file. Please try again.");
-      }
-      setUploading(false);
-    }
+  const handleUpload = () => {
+    uploadImage(selectedImage, resetFileInput);
   };
 
   const handleCancel = () => {
+    resetFileInput();
+  };
+
+  const resetFileInput = () => {
     setSelectedImage(null);
-    fileInputRef.current.value = "";
+    if (fileInputRef.current) fileInputRef.current.value = "";
   };
 
   return (
@@ -59,7 +31,7 @@ const UploadPicture = ({ gameId, user, onUpload }) => {
         <h2 className="mt-5">Upload A Picture</h2>
         <FileInput
           fileInputRef={fileInputRef}
-          onFileSelect={handleFileSelect}
+          setSelectedImage={setSelectedImage}
         />
         <ImagePreview selectedImage={selectedImage} onCancel={handleCancel} />
         {error && (
