f5760f1ce77093dfe3e0b828807da82c97d0456a
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-02-04 16:08:37 +0100

    refactor: created useFetchImages hook and moved fetchImage logic from PhotoGallery into useFetchImages

---
+++ src/hooks/useFetchImages.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { storage } from "../firebase";
+import { ref, getDownloadURL, listAll, getMetadata } from "firebase/storage";
+
+export const useFetchImages = (gameId) => {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const imageRefs = await listAll(ref(storage, `pictures/${gameId}`));
+        const urlsPromises = imageRefs.items.map(async (itemRef) => {
+          const url = await getDownloadURL(itemRef);
+          const metadata = await getMetadata(itemRef);
+          return { url, metadata };
+        });
+        const urls = await Promise.all(urlsPromises);
+        urls.sort(
+          (a, b) =>
+            new Date(a.metadata.timeCreated) - new Date(b.metadata.timeCreated)
+        );
+
+        setImages(urls);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        setError("Error fetching images. Please try again.");
+      }
+    };
+
+    fetchImages();
+  }, [gameId]);
+
+  const addImage = (newImage) => {
+    setImages((prevImages) => [...prevImages, newImage]);
+  };
+
+  return { images, error, addImage };
+};
--- src/views/PhotoGallery.js
+++ src/views/PhotoGallery.js
@@ -1,46 +1,14 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { storage } from "../firebase";
-import { ref, getDownloadURL, listAll, getMetadata } from "firebase/storage";
 import { Container, Alert } from "react-bootstrap";
 import UploadPicture from "../components/UploadPicture";
 import Gallery from "../components/Gallery";
 import { useAuthRedirect } from "../hooks/useAuthRedirect";
+import { useFetchImages } from "../hooks/useFetchImages";
 
 export const PhotoGallery = () => {
   const { gameId } = useParams();
   const user = useAuthRedirect();
-
-  const [error, setError] = useState(null);
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const imageRefs = await listAll(ref(storage, `pictures/${gameId}`));
-        const urlsPromises = imageRefs.items.map(async (itemRef) => {
-          const url = await getDownloadURL(itemRef);
-          const metadata = await getMetadata(itemRef);
-          return { url, metadata };
-        });
-        const urls = await Promise.all(urlsPromises);
-        urls.sort(
-          (a, b) =>
-            new Date(a.metadata.timeCreated) - new Date(b.metadata.timeCreated)
-        );
-
-        setImages(urls);
-      } catch (error) {
-        console.error("Error fetching images:", error);
-        setError("Error fetching images. Please try again.");
-      }
-    };
-    fetchImages();
-  }, [gameId]);
-
-  const handleUpload = (newImage) => {
-    setImages([...images, newImage]);
-  };
+  const { images, error, addImage } = useFetchImages(gameId);
 
   return (
     <Container className="text-center d-flex justify-content-center">
@@ -51,7 +19,7 @@ export const PhotoGallery = () => {
           </Alert>
         )}
         <Gallery images={images} />
-        <UploadPicture gameId={gameId} user={user} onUpload={handleUpload} />
+        <UploadPicture gameId={gameId} user={user} onUpload={addImage} />
       </div>
     </Container>
   );
