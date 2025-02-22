e8936362df40a1ce4100f04021d084b5befc4530
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2025-02-22 18:08:37 +0100

    feat: created useClickOutside custom hook and updated NavHeader so that nav dropdown closes when a user clicks outside of the dropdown or navigates to another page

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,34 +1,45 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../utils/asset/nysl_logo.png";
 import { SignIn } from "./SignIn";
+import { useClickOutside } from "../hooks/useClickOutside";
 
 export const NavHeader = ({ onNavbarToggle }) => {
   const [expanded, setExpanded] = useState(false);
+  const navbarRef = useRef(null);
+  const location = useLocation();
 
-  const handleToggle = () => {
-    setExpanded(!expanded);
-    onNavbarToggle(!expanded);
-  };
+  useClickOutside(navbarRef, () => {
+    setExpanded(false);
+    onNavbarToggle(false);
+  });
+
+  useEffect(() => {
+    setExpanded(false);
+    onNavbarToggle(false);
+  }, [location.pathname, onNavbarToggle]);
 
   return (
-    <div>
+    <div ref={navbarRef}>
       <Navbar
         expand="md"
         className="bg-body-tertiary"
         fixed="top"
         bg="dark"
         expanded={expanded}
-        onToggle={handleToggle}
+        onToggle={() => setExpanded(!expanded)}
       >
         <Container>
           <Navbar.Brand as={Link} to="/">
             <img src={logo} width={60} alt="NYSL Logo" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={() => setExpanded(!expanded)}
+          />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/schedule">
